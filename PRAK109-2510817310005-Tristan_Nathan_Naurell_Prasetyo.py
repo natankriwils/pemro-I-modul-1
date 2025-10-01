647349310a6d2a07dbfee72ad13653b047b2e880
natankriwill <aelfric310@gmail.com>
2025-10-01 13:59:07 +0800

    Delete PRAK109-2510817310005-Tristan_Nathan_Naurell_Prasetyo.py

--- PRAK109-2510817310005-Tristan_Nathan_Naurell_Prasetyo.py
+++
@@ -1,7 +0,0 @@
-pasukan = 958730
-pahlawan = 5
-musuh_per_pahlawan = pasukan // pahlawan
-
-print(f"Jumlah pasukan yang dibawa Yu Zhong = {pasukan}")
-print(f"Jumlah pahlawan = {pahlawan}")
-print(f"Jumlah pasukan yang harus dikalahkan setiap pahlawan adalah {musuh_per_pahlawan} pasukan")
